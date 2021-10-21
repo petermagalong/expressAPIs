d3e0e9d60b8b908881b93ab1e191112d212fc834
Peter Christian Magalong <zpmmagalong@917ventures.com>
2021-10-21 20:11:12 +0800

    handle rejections connection to db

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
+const morgan = require('morgan');
+const connectDB = require('./config/db')
 //Route files
 const bootcamps = require('./routes/bootcamps')
 
 //load env
 dotenv.config({ path: './config/config.env'});
 
+//connecct to database
+connectDB();
+
+
 const app = express();
+
+// const logger = (req, res, next) => {
+
+// }
+//Dev logging middleware
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
@@ -15,4 +28,10 @@ app.use('/api/v1/bootcamps', bootcamps);
 const PORT = process.env.PORT || 5000;
 
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
+process.on('unhandledRejection', (err,promise) => {
+    console.log(`Error: ${err.message}`);
+    //close server & exit process 
+    server.close(()=> process.exit(1));
+})
