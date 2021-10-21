43da1163f772e5fd01ae53fd7e3a892d1ab1c013
Peter Christian Magalong <zpmmagalong@917ventures.com>
2021-10-21 20:15:10 +0800

    insert npm colors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors')
 const connectDB = require('./config/db')
 //Route files
 const bootcamps = require('./routes/bootcamps')
@@ -28,10 +29,10 @@ app.use('/api/v1/bootcamps', bootcamps);
 const PORT = process.env.PORT || 5000;
 
 
-const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}` .yellow.bold));
 
 process.on('unhandledRejection', (err,promise) => {
-    console.log(`Error: ${err.message}`);
+    console.log(`Error: ${err.message}`.red);
     //close server & exit process 
     server.close(()=> process.exit(1));
 })
