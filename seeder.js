418a99e7cb03fa431144a2280233b7f0a43efd44
Peter Christian Magalong <zpmmagalong@917ventures.com>
2021-11-04 05:43:40 +0800

    course model

--- seeder.js
+++ seeder.js
@@ -7,7 +7,7 @@ dotenv.config({ path: './config/config.env' });
 
 // Load models
 const Bootcamp = require('./models/Bootcamp');
-
+const Course = require('./models/Course');
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -18,11 +18,15 @@ mongoose.connect(process.env.MONGO_URI, {
 const bootcamps = JSON.parse(
     fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
   );
+const courses = JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
+  );
 
   // Import into DB
 const importData = async () => {
     try {
       await Bootcamp.create(bootcamps);
+      await Course.create(courses)
       console.log('Data Imported...'.green.inverse);
       process.exit();
     } catch (err) {
@@ -33,13 +37,15 @@ const importData = async () => {
 const deleteData = async () => {
     try {
         await Bootcamp.deleteMany();
+        await Course.deleteMany();
         console.log('Data Destroy...'.red.inverse);
         process.exit();
     } catch (error) {
         console.log(error);
     }
 }
-
+// node seeder -d  ----- to delete
+// node seeder -i  ----- to insert
 if(process.argv[2] === '-i'){
     importData();
 }else if(process.argv[2] === '-d'){
